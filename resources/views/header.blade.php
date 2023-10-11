e1fd2d798b7561a82fa7675736ab1f422761866a
hana2001hana <127389484+hana2001hana@users.noreply.github.com>
2023-10-12 02:43:38 +0300

    edit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,18 +4,6 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/welcom">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">who are we</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">call us</a>
-                </li>
-            </ul>
-        </div>
+        
     </nav>
 </header>
